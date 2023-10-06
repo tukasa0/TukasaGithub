d2dd718c6b082109565c8a4fdba62608893034ec
sk2k0 <sk2k0@MSI>
2023-10-06 11:16:29 +0900

    Sum.hとSum.cppを追加

---
+++ Sum.cpp
@@ -0,0 +1,15 @@
+#include "Sum.h"
+#include <iostream>
+using namespace std;
+
+void Stream::Set()
+{
+	cout << "’l‚ð“ñ‚Â“ü—Í‚µ‚Ä‚­‚¾‚³‚¢" << endl;
+	cin >> a >> b;
+}
+
+int Stream::Sum()
+{
+	cout << "‘«‚µŽZ‚ÌŒ‹‰Ê‚Í";
+	return a + b;
+}
