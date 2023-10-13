9b3f722e9b28ff900dea760b45ca025ebbb83e3a
sk2k0 <sk2k0@MSI>
2023-10-13 11:27:50 +0900

    やり直し

--- TukasaGithub.cpp
+++ TukasaGithub.cpp
@@ -2,24 +2,3 @@
 #include <iostream>
 using namespace std;
 
-int mult(int a, int b)
-{
-	cout << "掛け算の結果は";
-	return a * b;
-}
-
-
-
-int main()
-{
-	
-	Stream stream;
-
-	stream.Set();
-	
-	cout << mult(stream.a, stream.b) << endl;
-
-	cout << stream.Sum();
-	
-}
-
